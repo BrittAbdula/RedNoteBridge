f9f4ed544f25e77b57ba7a96798c226db149e061
BrittAbdula <aurorozz@gmail.com>
2025-01-17 09:46:06 +0800

    sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://rednotebrige.com'
+  const baseUrl = 'https://rednotebridge.com'
   
   return [
     {
